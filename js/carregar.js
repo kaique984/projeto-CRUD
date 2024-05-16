385ad86445710bba61ef6bcff8ffee52b3c003fc
Logon Aluno <logonrmlocal@fiap.com.br>
2024-05-16 20:24:16 -0300

    projeto final2

--- js/carregar.js
+++ js/carregar.js
@@ -12,9 +12,9 @@ function card(tarefa){
                 <p>${tarefa.descricao}</p>
                 <a href="#" class="nes-badge is-icon">
                     <span class="is-warning"><i class="nes-icon coin is-small"></i></span>
-                    <span class="is-success">${tarefa.pontos}</span>
+                    <span class="is-success">${tarefa.status}</span>
                 </a> 
-                <progress class="nes-progress is-pattern" value="${tarefa.status}" max="100"></progress>
+                <progress class="nes-progress is-primary" value="80" max="100"></progress>
 
                 <button type="button" class="nes-btn is-primary">-</button>
                 <button onclick="apagar('${tarefa.id}')" type="button" class="nes-btn is-error">Apagar</button>
